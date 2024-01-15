d51c885861bac44ac8eebcc9ea70b90223c75a38
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-15 17:16:51 +0530

    updated with cors policy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("express");
 const connection = require("./connection");
 const userRoute = require("./routes/user.route");
 const { logout } = require("./controllers/user.controller");
@@ -10,6 +11,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoute);
 
